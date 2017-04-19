4ca078f33a6b45bc553bec9b633d8054862753be
Djurdja <Djrudja@Apples-iMac.local>
2017-04-19 12:19:05 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 var plumber = require('gulp-plumber');
 var coffee = require('gulp-coffee');
 var notify = require("gulp-notify");
@@ -19,7 +20,9 @@ gulp.task('sass', function () {
     gulp.src('./resources/scss/**/*.scss')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(sass())
+    .pipe(cssmin())
     .pipe(plumber.stop())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./public/css/'))
     .pipe(notify('Sass compiled'));
 });
@@ -28,10 +31,9 @@ gulp.task('scripts', function() {
     return gulp.src('./resources/js/**/*.js')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(concat('main.min.js'))
-    .pipe(gulp.dest('.public/js/'))
     .pipe(uglify())
     .pipe(plumber.stop())
-    .pipe(gulp.dest('.public/js/'))
+    .pipe(gulp.dest('./public/js/'))
     .pipe(notify('Javascript compiled'));
 });
 
@@ -39,3 +41,5 @@ gulp.task('watch', function() {
     gulp.watch('./resources/scss/**/*.scss', ['sass'])
     gulp.watch('./resources/js/**/*.js', ['scripts'])
 });
+
+gulp.task("default", ['watch']);
